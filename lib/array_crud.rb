9a7b3a48871851104698cd069a92620303431fe6
Alice Ly Ms <alicehly@gmail.com>
2019-07-08 20:43:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,9 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  pet_array = ["cat","dog","fish","turtle"]
-  pet_array << "toad"
-  p pet_array
+  animal_array = ["cat","dog","fish","turtle"]
+  animal_array << "toad"
+  p animal_array
 end
 
 def add_element_to_start_of_array(array, element)
