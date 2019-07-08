97477b42654eb356fcc7f5466ea8ae1bd445d9d3
Alice Ly Ms <alicehly@gmail.com>
2019-07-08 20:57:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array=[1,2,3,4]
+  array=["arrays!",4]
   array.pop "arrays!"
   p array
 end
