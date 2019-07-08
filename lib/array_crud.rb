892fe3420bb105e86ae4770f2212291af073c6dc
Alice Ly Ms <alicehly@gmail.com>
2019-07-08 20:17:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,6 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
+  array=[1,2,3,4]
   array << "5"
 end
 
@@ -19,7 +20,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.
 end
 
 def retrieve_element_from_index(array, index_number)
