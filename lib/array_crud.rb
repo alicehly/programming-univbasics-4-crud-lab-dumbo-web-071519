e3fc546a2d060930e985016248ae3bf2f4bdcc92
Alice Ly Ms <alicehly@gmail.com>
2019-07-08 20:48:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -37,8 +37,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
- color_array=["red", "blue", "green"]
- p color_array[2]
+ array=["red", "blue", "green"]
+ array[2]
 end
 
 def update_element_from_index(array, index_number, element)
