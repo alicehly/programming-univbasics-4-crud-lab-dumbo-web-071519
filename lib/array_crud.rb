8b6ab1b17e52e7de02fcbd5b06709c505e1f85d3
Alice Ly Ms <alicehly@gmail.com>
2019-07-08 21:01:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -34,7 +34,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
- array = ["red", "blue", "am"
+ array = ["red", "blue", "am"]
  
  array[3]
 end
