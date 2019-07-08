669daae5bfd7c3dc6f582b78f8c28f6f3e88ce73
Alice Ly Ms <alicehly@gmail.com>
2019-07-08 20:36:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   pet_array = ["cat","dog","fish","turtle"]
-  new_array = pet_array.push "toad"
+  new_array = pet_array << "toad"
   p new_array
 end
 
